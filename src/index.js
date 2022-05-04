65f3d19ad51091ca046a2998e0fe3fb171547b40
pericles842 <slouis482@mail.com>
2022-05-04 16:12:47 -0400

    Commit base

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+class Counter extends React.Component {
+  function HOla() {
+    return <div>
+      <p>hOLA</p>
+    </div>
+  }
+  render() {
+    HOla
+  }
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
